109829d3f76262cb027dd4644135f11dc3fe0d88
Sonia Mehta <743540@student.tor.loc>
2019-04-11 10:11:44 -0400

    first commit

--- mapscript.js
+++ mapscript.js
@@ -1,1 +1,21 @@
-var
+var map1,map2,map3;
+function initialize(){
+    var mapOptions={
+        zoom:2,
+        center:{lat:40.758904,lng:-73.985128}
+    };
+    var mapOptions2={
+        zoom:10,
+        center:{lat:40.758904,lng:-73.985128}
+    };
+    var mapOptions3={
+        zoom:18,
+        center:{lat:40.758904,lng:-73.985128}
+    };
+    map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
+    map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
+    map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
+}
+}
+
+}
